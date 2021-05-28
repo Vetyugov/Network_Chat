5227374c3fbd6caeb2c4db87d4f181f8d5586dcb
Nikita Vetyugov <nickitavet1998@gmail.com>
2021-05-28 10:17:57 +0300

    Сетевой чат с подключением к БД и возможностью смены ника. Если в коммите не будет БД, то на первом запуске сервера необходимо раскоментитить метод jdbcManager.writeDB(); на 30 строке класса MyServer

--- src/main/java/Level_3_Lesson_2/Client/Server/ClientHandler.java
+++ src/main/java/Level_3_Lesson_2/Client/Server/ClientHandler.java
@@ -1,5 +1,9 @@
 package Level_3_Lesson_2.Client.Server;
+<<<<<<< HEAD
 //Класс клиента.
+=======
+//Класс клиента
+>>>>>>> d508d91 (Сетевой чат с подключением к БД и возможностью смены ника. Если в коммите не будет БД, то на первом запуске сервера необходимо раскоментитить метод jdbcManager.writeDB(); на 30 строке класса MyServer)
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
