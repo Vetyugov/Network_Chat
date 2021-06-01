c279e785452e0310dabe9621ff4a7fc46ebc1068
Nikita Vetyugov <nickitavet1998@gmail.com>
2021-06-01 19:07:49 +0300

    Level_3_Lesson_4_part_1 3 потока выводят строку букв ABC

--- src/main/java/Level_3_Lesson_4/PrintABCApp.java
+++ src/main/java/Level_3_Lesson_4/PrintABCApp.java
@@ -6,15 +6,9 @@ public class PrintABCApp {
 
     public static void main(String[] args) {
         PrintABCApp waitNotifyObj = new PrintABCApp();
-        Thread thread1 = new Thread(() -> {
-            waitNotifyObj.printA();
-        });
-        Thread thread2 = new Thread(() -> {
-            waitNotifyObj.printB();
-        });
-        Thread thread3 = new Thread(() -> {
-            waitNotifyObj.printC();
-        });
+        Thread thread1 = new Thread(waitNotifyObj::printA);
+        Thread thread2 = new Thread(waitNotifyObj::printB);
+        Thread thread3 = new Thread(waitNotifyObj::printC);
         thread1.start();
         thread2.start();
         thread3.start();
